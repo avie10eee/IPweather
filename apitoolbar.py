464832144dbcb3618bc7172f7c8b2fbd06bed025
avie10eee <105246539+avie10eee@users.noreply.github.com>
2022-08-18 15:08:46 -0400

    Add files via upload

--- apitoolbar.py
+++ apitoolbar.py
@@ -2,17 +2,25 @@
 from requests import get
 import json
 
-from requests import get
-words = input("what would you like to translate to doge speak ")
-word = words.replace(" ", "%20")
-reponse = get('https://api.funtranslations.com/translate/doge.json?text=' + word)
-transl = json.loads(reponse.content.decode())
-print(transl)
-''''
-response = get("https://icanhazdadjoke.com/", headers={"Accept": "application/json"})
+ny = input("do you want to translate some thing y/n ")
+if ny == "y":
+    words = input("what would you like to translate to doge speak ")
+    word = words.replace(" ", "%20")
+    reponse = get('https://api.funtranslations.com/translate/doge.json?text=' + word)
+    transl = json.loads(reponse.content.decode())
+    print(transl["contents"]['translated'])
+else:
+    print("")
 
-joke = json.loads(response.content.decode())
+jy = input ("do you want a dad joke y/n ")
+if jy == "y":
+    response = get("https://icanhazdadjoke.com/", headers={"Accept": "application/json"})
+    joke = json.loads(response.content.decode())
+    print(joke["joke"])
+else:
+    print("")
 
-
-print(joke["joke"])
-'''
+yn = input("do you want know the nasa mars weather y/n ")
+if yn == "y":
+    marsw = get ("https://api.nasa.gov/insight_weather/?api_key=XcdlNOSRAYU8OF96PKLLho7Ib7XwTO1V5EK1KeTe&feedtype=json&ver=1.0")
+    print(json.loads(marsw.content.decode()))
