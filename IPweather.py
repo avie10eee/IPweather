a398b1bcbd4307cf495fe28f79cfc6032cceb638
avie10 <105246539+avie10eee@users.noreply.github.com>
2022-08-30 15:12:42 -0400

    Update IPweather.py

--- IPweather.py
+++ IPweather.py
@@ -5,26 +5,6 @@ import requests
 from requests import get
 import json
 
-ny = input("do you want to translate some thing y/n ")
-if ny == "y":
-    words = input("what would you like to translate to doge speak ")
-    word = words.replace(" ", "%20")
-    reponse = get('https://api.funtranslations.com/translate/doge.json?text=' + word)
-    transl = json.loads(reponse.content.decode())
-    print(transl["contents"]['translated'])
-
-
-jy = input ("do you want a dad joke y/n ")
-if jy == "y":
-    respose = get("https://icanhazdadjoke.com/", headers={"Accept": "application/json"})
-    joke = json.loads(respose.content.decode())
-    print(joke["joke"])
-
-yn = input("do you want know the mars weather y/n ")
-if yn == "y":
-    marsw = get ("https://api.nasa.gov/insight_weather/?api_key=XcdlNOSRAYU8OF96PKLLho7Ib7XwTO1V5EK1KeTe&feedtype=json&ver=1.0")
-    print(json.loads(marsw.content.decode()))
-
 
 def get_ip():
     response = requests.get('https://api64.ipify.org?format=json').json()
